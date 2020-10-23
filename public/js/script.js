43a66cd9d59e069e6460987ef19e7bfeb57a892c
Katarzyna Niemiec <katarzyna.niemiec.programista@gmail.com>
2020-10-23 16:59:24 +0200

    refactoring JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,22 @@
-window.onload = function() 
-{
+window.onload = () => {
 	var title = document.getElementById("title").innerHTML;
-	if(title === "Dodawanie wydatku" || title === "Dodawanie przychodu") setCurrentDate();
+	if (title === "Dodawanie wydatku" || title === "Dodawanie przychodu") setCurrentDate();
 }
 
-function setCurrentDate()
-{
+setCurrentDate = () => {
 	var dateOnPage = document.getElementById("date1");
-	if(dateOnPage.value == ""){
+	if (dateOnPage.value == "") {
 		var today = new Date();
 
 		var day = today.getDate().toString();
 		day = (day.length === 1) ? '0' + day : day;
-		
+
 		var month = today.getMonth() + 1;
 		month = (month.toString().length === 1) ? '0' + month : month;
-		
+
 		var year = today.getFullYear();
-	
-		dateOnPage.value = year + "-" + month + "-" + day ;
+
+		dateOnPage.value = year + "-" + month + "-" + day;
 
 	}
 }
